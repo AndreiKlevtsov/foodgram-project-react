f713ae214394707c06f2224e74bdbde5001fb6bc
Andrey Klevtsov <andrewklevt@gmail.com>
2023-08-14 11:19:24 +0300

    isort

--- backend/core/validators.py
+++ backend/core/validators.py
@@ -7,13 +7,13 @@ def validate_username(value):
     """
     Проверяет, что username пользователя != 'me'.
     """
-    REGEX = r'^[\w.@+-]'
+    regex = r'^[\w.@+-]'
     if value == 'me':
         raise ValidationError(
             (f'{value} не может быть <me>.'),
             params={'value': value},
         )
-    if not re.match(REGEX, value):
+    if not re.match(regex, value):
         raise ValidationError(
             (f'{value} не соотвествует допустимому формату.'),
             params={'value': value},
@@ -24,8 +24,8 @@ def validate_tag_slug(value):
     """
     Проверяет, slug field модели Tag.
     """
-    REGEX = r'^[-a-zA-Z0-9_]+$'
-    if not re.match(REGEX, value):
+    regex = r'^[-a-zA-Z0-9_]+$'
+    if not re.match(regex, value):
         raise ValidationError(
             (f'{value} не соотвествует допустимому формату.'),
             params={'value': value},
