003b11e505406ff0b6fe53e787cd1052d32e3eb6
Andrey Klevtsov <andrewklevt@gmail.com>
2023-08-17 11:12:47 +0300

    ci/cd

--- backend/foodgram_backend/settings.py
+++ backend/foodgram_backend/settings.py
@@ -143,7 +143,8 @@ STATIC_ROOT = BASE_DIR / 'static'
 # STATIC_ROOT = BASE_DIR / 'collected_static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_ROOT = BASE_DIR / MEDIA_URL
+# MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
